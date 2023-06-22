d86a51673295b398eba0044977ffe2b3212eb9db
jvill171 <juanvillarreal64@yahoo.com>
2023-06-21 23:59:54 -0500

    Modified file such that process.env.PGPASSWORD is set up using a .env variable

--- config.js
+++ config.js
@@ -1,12 +1,26 @@
 /** Common config for message.ly */
 
 // read .env files and make environmental variables
+const PG_PWD = require('./.env')
 
 require("dotenv").config();
+
+process.env.PGPASSWORD = PG_PWD
+
 
 const DB_URI = (process.env.NODE_ENV === "test")
   ? "postgresql:///messagely_test"
   : "postgresql:///messagely";
+
+// const DB_URI = (process.env.NODE_ENV === "test")
+//   ? {
+//     host:"/var/run/postgresql/",
+//     database: "messagely_test"
+//   }
+//   : {
+//     host:"/var/run/postgresql/",
+//     database: "messagely"
+//   }
 
 const SECRET_KEY = process.env.SECRET_KEY || "secret";
 
